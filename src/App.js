4c0ef7c7bdb28c9834fbd1948b2444ca3616f76b
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2021-01-07 10:40:53 +0100

    fix: changes images background links

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import About from './components/About';
-import SearchBar from './components/SearchBar';
-import Footer from './components/Footer';
-import styled from 'styled-components';
-import ScrollButton from './components/ScrollButton';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./components/About";
+import SearchBar from "./components/SearchBar";
+import Footer from "./components/Footer";
+import styled from "styled-components";
+import ScrollButton from "./components/ScrollButton";
 
 const today = new Date();
 const Background = styled.div`
-    background: ${({ time = today.getHours() }) =>
-
-    (time >= 6 && time < 12 && 'url("https://img-weather.netlify.app/background/background_dawn2.jpg")') ||
-    (time >= 12 && time < 18 && 'url("https://img-weather.netlify.app/background/background_day3.jpg")') ||
-    (time >= 18 && time <= 23 && 'url("https://img-weather.netlify.app/background/background_dusk3.jpg")') ||
-    (time >= 0 && time < 6 && 'url("https://img-weather.netlify.app/background/background_night7.jpg")')
-  } no-repeat fixed center;
-    min-height: 100vh;
-    background-size: cover;`;
+  background: ${({ time = today.getHours() }) =>
+      (time >= 6 &&
+        time < 12 &&
+        'url("https://i.ibb.co/Ks1VVmM/background-dawn2.jpg")') ||
+      (time >= 12 &&
+        time < 18 &&
+        'url("https://i.ibb.co/52Q31JW/background-day3.jpg")') ||
+      (time >= 18 &&
+        time <= 23 &&
+        'url("https://i.ibb.co/zmnq4Wm/dusk.jpg")') ||
+      (time >= 0 &&
+        time < 6 &&
+        'url("https://i.ibb.co/2Yj0L9Q/background-night7.jpg")')}
+    no-repeat fixed center;
+  min-height: 100vh;
+  background-size: cover;
+`;
 
 class App extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.myRef = React.createRef();
     this.state = {
-      scrollButton: false
+      scrollButton: false,
     };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     const scrollComponent = this;
-    document.addEventListener('scroll', function (e) {
+    document.addEventListener("scroll", function (e) {
       scrollComponent.toggleVisibility();
     });
   }
@@ -41,36 +49,33 @@ class App extends React.Component {
     } else {
       this.setState({ scrollButton: false });
     }
-  }
+  };
 
-  scrollToTop () {
+  scrollToTop() {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   }
 
-  render () {
+  render() {
     return (
       <Router>
-
-        <main
-          ref={this.myRef}
-          onScroll={this.handleScroll} id='App'
-        >
+        <main ref={this.myRef} onScroll={this.handleScroll} id="App">
           <Switch>
-            <Route exact path='/'>
+            <Route exact path="/">
               <Background>
-
                 <SearchBar />
                 <Footer />
               </Background>
             </Route>
-            <Route path='/about'>
+            <Route path="/about">
               <About />
             </Route>
           </Switch>
-          {this.state.scrollButton && <ScrollButton onClick={() => this.scrollToTop()} />}
+          {this.state.scrollButton && (
+            <ScrollButton onClick={() => this.scrollToTop()} />
+          )}
         </main>
       </Router>
     );
